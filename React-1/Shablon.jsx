d05b515caa2ec53a7f390bf20fd47a502f567a9a
IvanKalyuzhko <kalyuzhkoivan99@gmail.com>
2024-02-28 12:15:46 +0200

    add

---
+++ React-1/Shablon.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Shablon = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Shablon
--- React-4/src/App.js
+++ React-4/src/App.js
@@ -4,6 +4,7 @@ import ClassCounter from './components/СlassCounter';
 import "./styles/App.css"//імпортували стилі CSS
 import PostItem from './components/Postitem';
 import Postlist from './components/Postlist';
+import MyButton from './components/UI/Button/MyButton'
  
 function App() {
     const [posts,setPosts] = useState ( [
@@ -11,16 +12,15 @@ function App() {
       {id:2, title:"EXEMPLE props 2" , body :"можем прописувати любі дані" },
       {id:3, title:"EXEMPLE props 3" , body :"любого компоненту де звертаємось через props" },
     ]) // Створили стан із масивом постів
-    const [posts2,setPosts2] = useState ( [
-      {id:1, title:"Список постів 2" , body :"Тут опис самого посту" },
-      {id:2, title:"Список постів 2" , body :"можем прописувати любі дані" },
-      {id:3, title:"Список постів 2" , body :"любого компоненту де звертаємось через props" },
-    ]) // Створили стан із масивом постів
     
   return (
      <div className="App"> 
+      <form>
+        <input type="text" placeholder='Назва посту'/>
+        <input type="text" placeholder='Опис посту'/>
+        <MyButton >Створити пост</MyButton>
+      </form>
          <Postlist posts={posts} title="Список постів 1"/>
-         <Postlist posts={posts2 } title="Список постів 2"/>
      </div>
   )
 }
