c1707ba57f4614e1fc13ee4bdd746d08beffbaa9
IvanKalyuzhko <kalyuzhkoivan99@gmail.com>
2024-03-01 12:13:13 +0200

    notes

---
+++ React-6/src/App.js
@@ -0,0 +1,65 @@
+import React, { useRef, useState } from 'react'
+import Counter from './components/Counter';
+import ClassCounter from './components/СlassCounter';
+import "./styles/App.css"//імпортували стилі CSS
+import PostItem from './components/Postitem';
+import Postlist from './components/Postlist';
+import MyButton from './components/UI/Button/MyButton'
+import MyInput from './components/UI/Input/MyInput'
+ 
+function App() {
+    const [posts,setPosts] = useState ( [
+      {id:1, title:"EXEMPLE props" , body :"Тут опис самого посту" },
+      {id:2, title:"EXEMPLE props 2" , body :"можем прописувати любі дані" },
+      {id:3, title:"EXEMPLE props 3" , body :"любого компоненту де звертаємось через props" },
+    ]) // Створили стан із масивом постів
+
+    const [title,setTitle] = useState ("назва в середині")
+    const [body, setBody] = useState ("")
+
+    const addNewPost = (e) =>{
+      e.preventDefault()//цією функцією ми запобігаєм default-ну поведінку браузера (щоб синхронізувати свіже вписану інформацію користувача )
+      //Тобто цією функцією запобігаємо обновленню сторінки при нажатті кнопки
+      const newPost = {//створюєм новий пост 
+        id:Date.now(), //тут ми отримуємо id(він завжди має бути унікальний) із поточної дати
+        title,  //передаєм заголовок у новий масив 
+        body  //передаєм тіло у новий масив 
+      }
+      setPosts([...posts,newPost])//...posts - цим значенням ми розвертаєм старий масив
+      //тут ми верхній обєкт додаєм у масив постів
+      setTitle("")
+      setBody("")
+      //після створення посту буди очищатися input (обнуляєм)
+     }
+    
+    
+  return (
+     <div className="App"> 
+      <form>
+        {/*Управляємий компонент*/}
+        <MyInput 
+        value={title}
+        onChange={e => setTitle(e.target.value)/*цією функцією ми відслідковуємо що користувач вводить*/}
+        type="text" 
+        placeholder='Назва посту'/>
+        {/*Тепер ми получаєм дані за допомогою керуючого Input */}
+        {/*Неуправляємий/неконтролюючий компонент */}
+        <MyInput
+         value={body}
+         onChange={e => setBody(e.target.value)/*цією функцією ми міняєм стан */}
+        type="text"
+        placeholder='Опис посту'/>
+        <MyButton onClick={addNewPost /*додаєм функцію*/}>Створити пост</MyButton>
+      </form>
+         <Postlist posts={posts} title="Список постів 1"/>
+     </div>
+  )
+}
+
+export default App;
+
+//Хуки - певні функції ,які дає React . Їх можна використовувати у функціональних компонентах або у своїх особистих хуках 
+//Хуки можна використовувати тільки на верхньому рівні вкладеності (тобто ми не можем вкладати хуки у певні функції/цикли/умови)
+
+//useState - хук управління станом
+//useRef - хук за допомогою якого ми можем отримувати напряму доступ до дом-елемента (маніпулювати дом-деревом напряму не рекомендують в Реакт ,але є ситуації коли це необхідно)
---
+++ React-6/src/components/Postlist.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import PostItem from "./Postitem"
+
+const Postlist = ({posts,title}) => {
+    return (
+        <div>
+            <h1 style={{textAlign:"center"}}>{/*додаєм стиль CSS- style={{textAlign:"center"}} */}
+               {title}
+            </h1> 
+           {posts.map((post,index) => //тут масив звичайних обєктів ми переробили в масив React елементів через функцію map
+            <PostItem number={index+1} post={post} key={post.id}/> // як правило ми використовуєм id обєкта яке використовуєм до key
+             // коли ми створюєм списки то обовязковою умовою є вказування певного key (цей key має зберігати певне унікальне значення)
+             //Ключі (key) дозволяють алгоритмам Реакту найбільш ефективно робити рендеринг і перерисовувати не весь список , а тільки ті елементи в яких відбулися зміни 
+            )/*Номер елемента у масиві ми отримуєм за допомогою index*/ }
+        </div>
+    )
+}
+
+export default Postlist
